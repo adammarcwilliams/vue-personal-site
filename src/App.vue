f83ac4caca554733d7c28e3b963c98b81b8967a2
Adam Marc Williams <adam@adammarcwilliams.co.uk>
2018-04-02 18:09:01 +0100

    Starts layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="details">
+      <div class="name">Adam Marc Williams</div>
+      <div class="job-title">Interactive Developer</div>
+      <div class="company">@rhythm.digital</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
-<style>
+<style lang='scss' scoped>
+@import './utils/scss/index.scss';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: $dark-bg;
 }
+
+.details {
+  position: relative;
+  width: 290px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+  color: $light-copy;
+  font-family: $ptsans;
+  margin-right: 10vw;
+}
+
+.name {
+  font-family: $oswald;
+  font-size: 30px;
+}
+
+.job-title {
+  font-size: 24px;
+  margin-top: 5px;
+}
+
+.company {
+  font-size: 20px;
+  margin-top: 5px;
+}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 
-Vue.config.productionTip = false
+// Include CSS Resets before any component styles
+import './utils/normalize.css';
+import './utils/base.css';
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
+});
