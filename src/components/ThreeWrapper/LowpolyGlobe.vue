778b36bd8c5182719f0e89d352f35b3e38f41e08
Adam Marc Williams <adam@adammarcwilliams.co.uk>
2018-04-08 11:38:15 +0100

    Changes LowPoly Globe

--- src/components/ThreeWrapper/LowpolyGlobe.vue
+++ src/components/ThreeWrapper/LowpolyGlobe.vue
@@ -4,6 +4,11 @@ import * as THREE from 'three'
 export default {
   name: 'LowpolyGlobe',
   inject: ['global'],
+  data () {
+    return {
+      speed: 0.01
+    }
+  },
   mounted () {
     this.geometry = new THREE.IcosahedronGeometry(1, 1)
     this.material = new THREE.MeshPhongMaterial({ color: 0x2194ce, flatShading: true })
@@ -13,7 +18,7 @@ export default {
   methods: {
     loop () {
       console.log('Object 3D recieved tick')
-      this.mesh.rotation.y += 0.01
+      this.mesh.rotation.y += this.speed
     }
   },
   render: h => h()
